de591ce66263629040f0b50f81e5f7edc7684ddc
Victor Velev <victorivelev@gmail.com>
2019-12-30 03:56:24 +0200

    optimize EM-GMM + add Res

--- neural/mixq_vae.py
+++ neural/mixq_vae.py
@@ -7,6 +7,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from .convolutional import ResEncoder, ResDecoder
+
 __all__ = [
     'GMEmbedding',
     'MixqVAE',
@@ -14,95 +16,116 @@ __all__ = [
 
 
 class GMEmbedding(nn.Module):
+    '''Gaussian Mixture Model Embeddings
+    
+    GMM PyTorch optimization taken from:
+    https://angusturner.github.io/generative_models/2017/11/03/pytorch-gaussian-mixture-model.html
 
-    def __init__(self, embedding_dim, beta=0.33, lmbd=0.25, tolerance=1e-8, em_iter=3, num_embeddings=8):
+    Legend:
+    -----------
+    mu : mean
+    logvar : log scale variance
+    posterior : pi
+    
+    Parameters:
+    -----------
+     : 
+    
+    '''
+
+    log_norm_constant = -0.5 * math.log(2 * math.pi)
+
+    def __init__(self, embedding_dim=4, beta=0.9, lmbd=0.25, num_iter=3, num_embeddings=8):
         super(GMEmbedding, self).__init__()
         self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         self.embedding_dim = embedding_dim
         self.beta = beta
         self.lmbd = lmbd
-        self.tolerance = tolerance
-        self.em_iter = 3
+        self.num_iter = num_iter
         self.num_embeddings = num_embeddings
 
-        self.embeddings = nn.Embedding(num_embeddings, embedding_dim)
-        self.embeddings.weight.data.uniform_(-1/num_embeddings, 1/num_embeddings)
+        # Gaussian Embeddings
+        self.embeddings_mu = nn.Embedding(num_embeddings, embedding_dim)
+        self.embeddings_mu.weight.data.uniform_(-1/num_embeddings, 1/num_embeddings)
 
-        self.priors = []
-        self.gaussians_params = []
-        self._init_random_gaussians()
+        self.embeddings_logvar = nn.Embedding(num_embeddings, embedding_dim)
+        self.embeddings_logvar.weight.data.uniform_(-1/num_embeddings, 1/num_embeddings)
 
-        self.scaled_probabilites = []
-        self._max_probs = []
+        self.embeddings_pi = nn.Embedding(num_embeddings, num_embeddings)
+        self.embeddings_pi.weight.data.uniform_(-1/num_embeddings, 1/num_embeddings)
 
-    def _init_random_gaussians(self):
+        # Gaussian Batch-Parameters
+        self._batch_mu = []
+        self._batch_logvar = []
+        self._batch_pi = []
+
+        self._init_random_batch_gaussians()
+
+    def _init_random_batch_gaussians(self):
         '''Initialize gaussians randomly'''
 
-        self.priors = (1 / self.num_embeddings) * torch.ones(self.num_embeddings, requires_grad=False, device=self._device)
-        for _ in range(self.num_embeddings):
-            params = {}
-            params['mean'] = torch.randn(self.embedding_dim, requires_grad=False, device=self._device)
-            params['cov'] = torch.eye(self.embedding_dim, requires_grad=False, device=self._device)
-            self.gaussians_params.append(params)
+        self._batch_mu = torch.randn(self.num_embeddings, self.embedding_dim, requires_grad=False, device=self._device)
+        self._batch_logvar = torch.ones(self.num_embeddings, self.embedding_dim, requires_grad=False, device=self._device)
+        self._batch_pi = torch.empty(self.num_embeddings, self.num_embeddings, requires_grad=False, device=self._device).fill_(1. / self.num_embeddings)
 
-    def likelihood(self, x, params):
-        mean = params['mean']
-        covar = params['cov']
+    def log_gaussian(self, x, mu, logvar):
+        logexp = -0.5 * (logvar + ((x - mu) ** 2) / logvar.exp())
+        return logexp + GMEmbedding.log_norm_constant
 
-        likelihood = torch.zeros(x.shape[0], requires_grad=False, device=self._device)
-
-        for i, sample in enumerate(x):
-            coeff = 1.0 / torch.sqrt(((2.0 * math.pi) ** self.embedding_dim) * torch.det(covar))
-            exponent = torch.exp(-0.5 * (sample - mean).T @ torch.pinverse(covar) @ (sample - mean))
-            likelihood[i] = coeff * exponent
-
-        return likelihood
-
-    def _get_likelihoods(self, x):
+    def _get_likelihoods(self, x, log=True):
         '''Calculate the likelihood over all samples and gaussians'''
 
-        likelihoods = torch.zeros(x.shape[0], self.num_embeddings, requires_grad=False, device=self._device)
-        for i in range(self.num_embeddings):
-            likelihoods[:, i] = self.likelihood(x, self.gaussians_params[i])
+        # get feature-wise log-likelihoods (num_embeddings, examples, features)
+        log_likelihoods = self.log_gaussian(
+            x[None, :, :], # (1, examples, features)
+            self._batch_mu[:, None, :], # (num_embeddings, 1, features)
+            self._batch_logvar[:, None, :] # (num_embeddings, 1, features)
+        )
 
-        return likelihoods
-    
-    def _expectation(self, x):
-        '''Calculate the probabilites'''
+        # sum over the feature dimension
+        log_likelihoods = log_likelihoods.sum(-1)
 
-        with torch.no_grad():
-            # Calculate probabilities of x belonging to the different gaussians
-            probabilities = self._get_likelihoods(x) * self.priors
-            sum_probabilities = probabilities.sum(1, keepdim=True)
+        if not log:
+            return log_likelihoods.exp()
 
-            self.scaled_probabilites = probabilities / sum_probabilities
-            # Save value for convergence check
-            self._max_probs.append(self.scaled_probabilites.max(1).values)
+        return log_likelihoods
 
-    def _maximization(self, x):
-        '''Update the parameters and priors'''
+    def _logsumexp(self, x, dim, keepdim=False):
+        max_values = x.max(dim=dim, keepdim=True).values
+        return max_values + (x - max_values).exp().sum(dim=dim, keepdim=keepdim).log()
 
-        with torch.no_grad():
-            # Iterate through clusters and recalculate mean and covariance
-            for i in range(self.num_embeddings):
-                weights = self.scaled_probabilites[:, i].unsqueeze(1)
-                mean = (weights * x).sum(0) / weights.sum()
-                covariance = ((x - mean).T @ ((x - mean) * weights)) / weights.sum()
-                self.gaussians_params[i]['mean'] = self.beta * self.gaussians_params[i]['mean'] + (1 - self.beta) * mean
-                self.gaussians_params[i]['cov'] = self.beta * self.gaussians_params[i]['cov'] + (1 - self.beta) * covariance
+    def _get_posteriors(self, log_likelihoods, log=True):
+        log_posteriors = log_likelihoods - self._logsumexp(log_likelihoods, dim=0, keepdim=True)
 
-            # Update weights
-            self.priors = self.beta * self.priors + (1 - self.beta) * self.scaled_probabilites.sum(0) / x.shape[0]
+        if not log:
+            return log_posteriors.exp()
 
-    def _converged(self):
-        '''Covergence if || max_prob - last_max_prob || < tolerance'''
+        return log_posteriors
 
-        if len(self._max_probs) < 2:
-            return False
+    def _update_batch_parameters(self, x, eps=1e-6, min_var=1e-6):
+        posteriors = self._get_posteriors(self._get_likelihoods(x), log=False)
 
-        diff = torch.norm(self._max_probs[-1] - self._max_probs[-2])
-        return diff <= self.tolerance
+        # compute `N_k` the proxy "number of points" assigned to each distribution.
+        N_k = torch.sum(posteriors, dim=1)
+        N_k = N_k.view(self.num_embeddings, 1, 1)
+
+        # get the means by taking the weighted combination of points
+        # (K, 1, examples) @ (1, examples, features) -> (K, 1, features)
+        mu = posteriors[:, None] @ x[None, ]
+        mu = mu / (N_k + eps)
+        self._batch_mu = mu.squeeze(1)
+
+        # compute the diagonal covar. matrix, by taking a weighted combination of
+        # the each point's square distance from the mean
+        A = x - mu
+        var = posteriors[:, None] @ (A ** 2) # (K, 1, features)
+        var = var / (N_k + eps)
+        logvar = var.clamp(min=min_var).log()
+        self._batch_logvar = logvar.squeeze(1)
+
+        # recompute the priors
+        self._batch_pi = (N_k / N_k.sum()).squeeze()
 
     def forward(self, x):
         # Convert inputs `x` from BCHW -> BHWC
@@ -112,29 +135,22 @@ class GMEmbedding(nn.Module):
         # Flatten input
         x_flatten = x.reshape(-1, self.embedding_dim)
 
-        # Run EM until convergence or for max iterations
-        for _ in range(self.em_iter):
-            self._expectation(x_flatten)    # E-step
-            self._maximization(x_flatten)   # M-step
-
-            # Check convergence
-            if self._converged():
-                break
-        
-        # Final assignment
-        self._expectation(x_flatten)
+        # Run EM for `num_iter` iterations
+        for _ in range(self.num_iter):
+            self._update_batch_parameters(x_flatten)
 
         # Update Embeddings
-        for i in range(self.num_embeddings):
-            self.embeddings.weight.data[i, :] = self.gaussians_params[i]['mean']
+        self.embeddings_mu.weight.data = self.beta * self.embeddings_mu.weight.data + (1 - self.beta) * self._batch_mu
+        self.embeddings_logvar.weight.data = self.beta * self.embeddings_logvar.weight.data + (1 - self.beta) * self._batch_logvar
+        self.embeddings_pi.weight.data = self.beta * self.embeddings_pi.weight.data + (1 - self.beta) * self._batch_pi
 
         # Encoding
-        encoding_indices = self.scaled_probabilites.argmax(1, keepdim=True)
+        encoding_indices = self._get_likelihoods(x_flatten, log=False).T.argmax(1, keepdim=True)
         encodings = torch.zeros(encoding_indices.shape[0], self.num_embeddings, requires_grad=False, device=self._device)
         encodings.scatter_(1, encoding_indices, 1)
         
         # Quantize
-        quantized = encodings @ self.embeddings.weight
+        quantized = encodings @ self.embeddings_mu.weight
         quantized_reshaped = quantized.reshape(input_shape).permute(0, 3, 1, 2)
         
         return encodings, quantized_reshaped
@@ -145,46 +161,29 @@ class GMEmbedding(nn.Module):
 
 class MixqVAE(nn.Module):
 
-    def __init__(self, embedding_dim, beta=0.9):
+    def __init__(self, in_channels=3, num_hiddens=128, num_res_hiddens=32, num_res_layers=2,
+                    embedding_dim=4, beta=0.9, lmbd=0.25, num_iter=3, num_embeddings=8, rgb=True):
+
         super(MixqVAE, self).__init__()
 
-        self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(16, 8, 3, stride=1, padding=1),
-            nn.ReLU(),
-        )
+        self.encoder = ResEncoder(in_channels, num_hiddens, num_res_hiddens, num_res_layers)
 
-        self.pre_vq_conv = nn.Sequential(
-            nn.Conv2d(8, embedding_dim, 3, stride=1, padding=1),
-            nn.Sigmoid(),
-        )
+        self.pre_conv = nn.Conv2d(num_hiddens, embedding_dim, 
+                kernel_size=1, stride=1)
 
-        self.gm_embedding = GMEmbedding(embedding_dim, beta=beta)
+        self.gm_embedding = GMEmbedding(embedding_dim, beta, lmbd, num_iter, num_embeddings)
 
-        self.post_vq_conv = nn.Sequential(
-            nn.ConvTranspose2d(embedding_dim, 8, 3, stride=1, padding=1),
-            nn.ReLU(),
-        )
-
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16, 3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, 3, stride=1, padding=1),
-            nn.Sigmoid()
-        )
+        self.decoder = ResDecoder(embedding_dim, num_hiddens, num_res_hiddens, num_res_layers, rgb=rgb)
 
     def forward(self, x):
-        encoded = self.pre_vq_conv(
+        encoded = self.pre_conv(
             self.encoder(x)
         )
 
         _, quantized = self.gm_embedding(encoded)
         quantized_skip_grad = encoded + (quantized - encoded).detach()
 
-        recon_x = self.decoder(
-            self.post_vq_conv(quantized_skip_grad)
-        )
+        recon_x = self.decoder(quantized_skip_grad)
 
         return encoded, quantized, recon_x
 
